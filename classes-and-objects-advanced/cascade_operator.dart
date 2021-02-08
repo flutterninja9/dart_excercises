2fa770061869ec13f0400d99a7b68fd05fed6d0e
Anirudh Pratap Singh <anirudhisgreat1200@gmail.com>
2021-02-08 18:07:14 +0530

    cascade_operator

--- classes-and-objects-advanced/cascade_operator.dart
+++ classes-and-objects-advanced/cascade_operator.dart
@@ -1,3 +1,28 @@
 // Cascade(..) operator is used in Dart to modify the properties of a mutable class
+// Just makes your code a bit less lengthy by providing shorthand for assignment operator
 
-void main() {}
+import 'dart:math';
+
+class ClosedPath {
+  List<Point> _points = [];
+
+  void moveTo(Point point) {
+    _points = [point];
+  }
+
+  void lineTo(Point point) {
+    _points.add(point);
+  }
+}
+
+void main() {
+  final closedPath = ClosedPath()
+    ..moveTo(Point(0, 0))
+    ..lineTo(Point(2, 0))
+    ..lineTo(Point(2, 2))
+    ..lineTo(Point(0, 2))
+    ..lineTo(Point(0, 0));
+  for (var point in closedPath._points) {
+    print(point);
+  }
+}
