8374cdf1ce90b18fc1b0f099f674a46321041481
Anirudh Singh <anirudhisgreat1200@gmail.com>
2021-02-18 17:32:52 +0530

    extension_trick

---
+++ mixins_and_extensions/iterable_sum.dart
@@ -0,0 +1,15 @@
+// Want to apply an extension to all the sub-classes of a base-class
+// Make use of a generic type constraint
+// You can do this by <T extends base_class>
+// Package Dartx provides more of such helpful extensions, Make sure to use it & go through the source code of it
+extension SumIterableX<T extends num> on Iterable<T> {
+  T sum() =>
+      reduce((currElement, nextElement) => currElement + nextElement as T);
+}
+
+void main() {
+  final listSumA = [1, 2, 3].sum();
+  final listSumB = [1.0, 2.0, 3.2].sum();
+  print(listSumA);
+  print(listSumB);
+}
